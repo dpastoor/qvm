9c59dce80899fa25fcb533e15486ab4bd0e84c22
Devin Pastoor <devin.pastoor@gmail.com>
2022-05-28 20:30:51 -0400

    refactor: normalize mac to follow linux .config convention since quarto doesn't handle spaces in path well
    see https://github.com/quarto-dev/quarto-cli/issues/1002 for more details

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"os"
+	"path/filepath"
+	"runtime"
 
+	"github.com/adrg/xdg"
 	"github.com/dpastoor/qvm/cmd"
 )
 
@@ -14,5 +17,12 @@ var (
 )
 
 func main() {
+	// normalize the config home on osx to linux and get rid of the path spacing
+	if runtime.GOOS == "darwin" {
+		hd, _ := os.UserHomeDir()
+		os.Setenv("XDG_CONFIG_HOME", filepath.Join(hd, ".config"))
+		os.Setenv("XDG_DATA_HOME", filepath.Join(hd, ".config"))
+	}
+	xdg.Reload()
 	cmd.Execute(version, os.Args[1:])
 }
